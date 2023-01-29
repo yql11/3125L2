c3aed4bb458d26ae53cf4680c0a18ad5439d1f12
atlzuki <94793271+atlzuki@users.noreply.github.com>
2023-01-29 08:59:31 +0800

    Add files via upload

--- scripts/groceries.js
+++ scripts/groceries.js
@@ -89,6 +89,9 @@ function restrictListProducts(prods, restriction) {
 		else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
 			product_names.push(prods[i].name);
 		}
+		else if ((restriction == "Meat") && (prods[i].meat == true)){
+			product_names.push(prods[i].name);
+		}
 		else if (restriction == "None"){
 			product_names.push(prods[i].name);
 		}
