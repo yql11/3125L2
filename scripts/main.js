f8a9d155d22a296a711bbd123848f30bf0f6a670
yql11 <65364852+yql11@users.noreply.github.com>
2023-02-03 17:11:04 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -88,6 +88,6 @@ function selectedItems(){
 		
 	// add paragraph and total price
 	c.appendChild(para);
-	c.appendChild(document.createTextNode("Total Price is $ " + getTotalPrice(chosenProducts)));
+	c.appendChild(document.createTextNode("Total Price is $  + getTotalPrice(chosenProducts)"));
 		
 }
